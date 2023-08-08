fcabb5350e893414b702d6716849678cb8b96d06
Cesar Izquierdo Tello <cesiztel@gmail.com>
2023-08-08 15:57:25 +0200

    testing

--- script.sh
+++ script.sh
@@ -1,23 +1,6 @@
-# Start building change log
-# 1. Get the previous tag
-PREVIOUS_TAG=$(git describe --abbrev=0 --tags $(git rev-list --tags --skip=1 --max-count=1))
-echo $PREVIOUS_TAG
-PR_RELEASE=$(git log --merges --grep 'Merge pull request' --pretty=format:%s $PREVIOUS_TAG..v1.6.0 | grep -o '#[0-9]*')
-
-# List of the PRS of the release $PR_RELEASE
-# Preparing notes template"
-NOTES="User story | Description \n -- | -- \n"
-
-# Looping over releaseses to get the data
-for pr_number in $PR_RELEASE
+header="| header | header |\n| ------ | ------ |\n"
+for i in *sh
 do
-    # Getting data from PR: ${pr_number}
-    change_log_entry_title=$(gh pr view ${pr_number} | head -n 1 )
-    # Stripping the title 
-    read lastElement <<< "${change_log_entry_title##*:}"
-    # Adding release to the notes
-    NOTES+="$lastElement \n"
+    header+="| ${i} |  |\n"
 done
-
-# Preparing output
-echo $NOTES
+printf "$header"
